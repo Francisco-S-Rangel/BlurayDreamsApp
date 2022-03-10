f427adcb3692b0a241e9e970d8c3e17d4f7a00e1
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-03-09 22:22:26 -0300

    html e css da tela-login feito

--- src/app/modules/cliente/telas-publicas/tela-login/tela-login.component.ts
+++ src/app/modules/cliente/telas-publicas/tela-login/tela-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tela-login',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela-login.component.css']
 })
 export class TelaLoginComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  backPage() { this.router.navigate(['']); }
 
 }
