69b35f77df12999fe42f3754fe6a3ce53651ce1f
MateRamires <matheushenriqueramires@gmail.com>
2022-03-19 01:31:46 -0300

    fix na tela de cartao

--- src/app/modules/cliente/finalizar-compra/finalizar-cartao/finalizar-cartao.component.ts
+++ src/app/modules/cliente/finalizar-compra/finalizar-cartao/finalizar-cartao.component.ts
@@ -11,22 +11,14 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 })
 export class FinalizarCartaoComponent implements OnInit {
 
-  public id?: number;
-  public idcliente: number = 0;
   public formCartao!: FormGroup;
 
   constructor(private router: Router, private fb: FormBuilder, private shared: SharedDataService
-  , private route: ActivatedRoute,private cartaoCreditoService: CartaoCreditoService, private cdRef: ChangeDetectorRef) { }
+    , private route: ActivatedRoute, private cartaoCreditoService: CartaoCreditoService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(x => {
-      this.id = x[`clienteid`];
-      this.idcliente = x[`clienteid`];
-    });
     this.validacao();
   }
-
-
 
   bandeiraCartao: any = [
     {
@@ -45,30 +37,14 @@ export class FinalizarCartaoComponent implements OnInit {
   cartaoCreditos =
     {
       id: 0,
-      clienteId: this.id,
+      clienteId: 0,
       numeroCartao: "",
       bandeiraCartao: "",
       cvv: "",
       nomeTitular: ""
     }
 
-
-
-
-  get f(): any {
-    return this.formCartao.controls;
-  }
-
-
-
-  cadastrarCartao(){
-    console.log(this.formCartao.value);
-    this.cartaoCreditoService.post(this.formCartao.value).subscribe(
-      ()=>{
-        console.log();
-        this.backPage();
-      }
-    );
+  cadastrarCartao() {
   }
 
   public validacao(): void {
@@ -78,14 +54,11 @@ export class FinalizarCartaoComponent implements OnInit {
     }
 
     this.formCartao = this.fb.group({
-      id: 0,
-      clienteid: this.idcliente,
       numeroCartao: ['', [Validators.required]],
       NomeTitular: ['', Validators.required],
       BandeiraCartao: ['', Validators.required],
       cvv: ['', Validators.required]
     }, formOptions)
-
 
   }
 
@@ -93,11 +66,11 @@ export class FinalizarCartaoComponent implements OnInit {
     return (group: AbstractControl) => {
       const formGroup = group as FormGroup
       const control = formGroup.controls[controlName]
-      let match:boolean = false
+      let match: boolean = false
 
       for (let i = 0; i < this.bandeiraCartao.length; i++) {
 
-        if (control.value.toLowerCase() == this.bandeiraCartao[i].bandeira){
+        if (control.value.toLowerCase() == this.bandeiraCartao[i].bandeira) {
           match = true
         }
 
@@ -107,8 +80,8 @@ export class FinalizarCartaoComponent implements OnInit {
         return null
       }
 
-      if(match != true){
-        control.setErrors({notValid: true})
+      if (match != true) {
+        control.setErrors({ notValid: true })
       } else {
         control.setErrors(null)
       }
@@ -118,6 +91,9 @@ export class FinalizarCartaoComponent implements OnInit {
     }
   }
 
+  get f(): any {
+    return this.formCartao.controls;
+  }
 
   backPage() {
     this.router.navigate(['/carrinho-compras']);
