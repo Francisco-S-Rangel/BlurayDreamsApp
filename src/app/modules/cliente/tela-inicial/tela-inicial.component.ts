ac188f195ab19baca1dc59e60d3039f0675c8ca2
MateRamires <matheushenriqueramires@gmail.com>
2022-03-23 01:09:48 -0300

    Tela inicial pegando os filmes do banco de dados

--- src/app/modules/cliente/tela-inicial/tela-inicial.component.ts
+++ src/app/modules/cliente/tela-inicial/tela-inicial.component.ts
@@ -1,3 +1,5 @@
+import { Produto } from './../../shared/models/produto';
+import { ProdutoService } from './../../shared/services/cadastro-dados-pedido/produto.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,27 +11,25 @@ import { Component, OnInit } from '@angular/core';
 export class TelaInicialComponent implements OnInit {
 
   public paginacao = [1,2,3];
-
-  public filmes = [
-    {img: 'https://www.curtocinema.com.br/wp-content/uploads/2021/08/taxi.png',titulo: 'Taxi Driver',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://pbs.twimg.com/media/FI_ZHvXUYAIWm_B?format=jpg&name=4096x4096',titulo: 'O poderoso Chefão',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://images.justwatch.com/poster/97524164/s718',titulo: 'Rocky: Um lutador',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://br.web.img2.acsta.net/medias/nmedia/18/92/91/08/20224693.jpg',titulo: 'O exterminador do futuro',dvd: '19,90',bluray: '39,90'},
-    {img: 'http://www.ecommercebrasil.com.br/wp-content/uploads/2014/01/o-lobo-de-wall-street.jpg',titulo: 'O exterminador do futuro',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://images.justwatch.com/poster/161964560/s592',titulo: 'Titanic',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://www.curtocinema.com.br/wp-content/uploads/2021/08/taxi.png',titulo: 'Taxi Driver',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://pbs.twimg.com/media/FI_ZHvXUYAIWm_B?format=jpg&name=4096x4096',titulo: 'O poderoso Chefão',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://images.justwatch.com/poster/97524164/s718',titulo: 'Rocky: Um lutador',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://br.web.img2.acsta.net/medias/nmedia/18/92/91/08/20224693.jpg',titulo: 'O exterminador do futuro',dvd: '19,90',bluray: '39,90'},
-    {img: 'http://www.ecommercebrasil.com.br/wp-content/uploads/2014/01/o-lobo-de-wall-street.jpg',titulo: 'O exterminador do futuro',dvd: '19,90',bluray: '39,90'},
-    {img: 'https://images.justwatch.com/poster/161964560/s592',titulo: 'Titanic',dvd: '19,90',bluray: '39,90'},
+  public produtos?: Produto[];
 
 
-  ];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private ProdutoService : ProdutoService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.carregarProdutos()
+  }
+
+
+  carregarProdutos(){
+    this.ProdutoService.getAll().subscribe(
+      (produtos: Produto[])=>{
+        this.produtos = produtos;
+        console.log(this.produtos);
+      }
+    );
+  }
 
   produtoSelecionado() {this.router.navigate(['produto-selecionado'])};
 
