9c51010198ac277e7fd2b9c801086cde3141059f
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-03-24 14:06:10 -0300

    Carrinho Service feito

---
+++ src/app/modules/shared/services/cadastro-dados-pedido/carrinho-compras.service.ts
@@ -0,0 +1,43 @@
+import { Carrinho } from './../../models/carrinho';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarrinhoComprasService {
+
+  baseUrl = `${environment.API_url}/Carrinhos`;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Carrinho[]>{
+    return this.http.get<Carrinho[]>(`${this.baseUrl}`);
+  }
+  getByClienteId(id: number): Observable<Carrinho[]>{
+    return this.http.get<Carrinho[]>(`${this.baseUrl}/${id}/carrinho`);
+  }
+  getById(id: number): Observable<Carrinho>{
+    return this.http.get<Carrinho>(`${this.baseUrl}/${id}`);
+  }
+  put(id: number,carrinho: Carrinho){
+    console.log(id);
+    console.log(carrinho);
+    return this.http.put(`${this.baseUrl}/${id}`, carrinho).pipe(catchError(this.errorHandler))
+  }
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error.message || "server error.");
+  }
+  post(carrinho: Carrinho){
+    return this.http.post(`${this.baseUrl}`, carrinho);
+  }
+  delete(id: number){
+    return this.http.delete<Carrinho>(`${this.baseUrl}/${id}`);
+  }
+
+
+}
