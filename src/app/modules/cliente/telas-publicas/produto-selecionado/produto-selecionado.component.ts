d27006330d6e01b0ffbd73ff8c57245f154309c7
MateRamires <matheushenriqueramires@gmail.com>
2022-03-24 17:56:44 -0300

    adicionar Produtos

--- src/app/modules/cliente/telas-publicas/produto-selecionado/produto-selecionado.component.ts
+++ src/app/modules/cliente/telas-publicas/produto-selecionado/produto-selecionado.component.ts
@@ -1,3 +1,5 @@
+import { Carrinho } from './../../../shared/models/carrinho';
+import { CarrinhoComprasService } from './../../../shared/services/cadastro-dados-pedido/carrinho-compras.service';
 import { Produto } from './../../../shared/models/produto';
 import { ProdutoService } from './../../../shared/services/cadastro-dados-pedido/produto.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,15 +13,46 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class ProdutoSelecionadoComponent implements OnInit {
 
   public id: number = 0;
-  public produto?: Produto;
+  public produto: Produto = {
+        id:0,
+        titulo:'',
+        img:'',
+        tipo:'',
+        categoria:'',
+        ano: new Date(),
+        direcao: '',
+        duracao: '',
+        produtora: '',
+        sinopse: '',
+        status: false,
+        preco: 0,
+        estoque: 0
+  };
+  public carrinho: Carrinho = {
+        id:0,
+        clienteId:0,
+        desconto:0,
+        frete:0,
+        precoFinal:0,
+  }
 
-  constructor(private router: Router, private route: ActivatedRoute, private ProdutoService: ProdutoService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private ProdutoService: ProdutoService, private CarrinhoComprasService: CarrinhoComprasService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(x=>{
       this.id = x[`id`];
       this.carregarProduto(this.id)
     })
+    this.carregarCarrinhoCliente(1)
+  }
+
+  carregarCarrinhoCliente(id: number){
+    this.CarrinhoComprasService.getById(id).subscribe(
+      (carrinho: Carrinho)=>{
+        this.carrinho = carrinho
+        console.log(this.carrinho)
+      }
+    )
   }
 
 
@@ -32,6 +65,16 @@ export class ProdutoSelecionadoComponent implements OnInit {
     );
   }
 
+  adicionarAoCarrinho(){
+    this.carrinho.Produtos = [this.produto]
+    this.CarrinhoComprasService.put(this.carrinho.id,this.carrinho).subscribe(
+      ()=>{
+        this.irParaCarrinho()
+      })
+  }
+
   backPage(){ this.router.navigate([''])};
+
+  irParaCarrinho(){ this.router.navigate(['carrinho-compras'])}
 
 }
