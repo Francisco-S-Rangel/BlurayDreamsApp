e0df958bb3fd485e7017e222767f9957dff2ab19
MateRamires <matheushenriqueramires@gmail.com>
2022-03-31 01:32:14 -0300

    Tela de trocas quase pronta

--- src/app/modules/cliente/finalizar-compra/finalizar-cupom-troca/finalizar-cupom-troca.component.ts
+++ src/app/modules/cliente/finalizar-compra/finalizar-cupom-troca/finalizar-cupom-troca.component.ts
@@ -1,6 +1,10 @@
+import { ProdutoService } from './../../../shared/services/cadastro-dados-pedido/produto.service';
+import { CarrinhoComprasService } from './../../../shared/services/cadastro-dados-pedido/carrinho-compras.service';
+import { Carrinho } from './../../../shared/models/carrinho';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, AbstractControlOptions, AbstractControl } from '@angular/forms';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Produto } from 'src/app/modules/shared/models/produto';
 
 @Component({
   selector: 'app-finalizar-cupom-troca',
@@ -11,16 +15,77 @@ export class FinalizarCupomTrocaComponent implements OnInit {
 
   public id?: number;
   public idcliente: number = 0;
-  public formTroca!: FormGroup;
 
-  constructor(private router: Router, private fb: FormBuilder
-  , private route: ActivatedRoute, private cdRef: ChangeDetectorRef) { }
+  public cuponsTroca: number = 1;
+  public arrayTrocas: number[] = [];
+  public qtdProdutosTrocas: number = 0
 
-  ngOnInit(): void {}
+  carrinho: Carrinho = {
+    id: 0,
+    clienteId: 0,
+    desconto: 0,
+    frete: 0,
+    precoFinal: 0,
+    carrinhoProduto: [{
+      id: 0,
+      carrinhoId: 0,
+      produtoId: 0,
+      quantidade: 0,
+      carrinho: new Carrinho(),
+      produto: new Produto()
+    }]
+  };
 
+  constructor(private router: Router, private fb: FormBuilder, private CarrinhoComprasService: CarrinhoComprasService, private ProdutoService: ProdutoService) { }
+
+  ngOnInit(): void {
+    this.carregarCarrinho()
+  }
+
+  carregarCarrinho() {
+
+    this.CarrinhoComprasService.getCarrinhoProdutos(1).subscribe((carrinho) => {
+      console.log(carrinho)
+      this.carrinho = carrinho
+
+      for (let i = 0; i < this.carrinho.carrinhoProduto!.length; i++) {
+        this.ProdutoService.getById(carrinho.carrinhoProduto![i].produtoId).subscribe((produto)=>{
+          this.carrinho.carrinhoProduto![i].produto = produto
+        })
+      }
+    })
+    //console.error = () => {};
+  }
+
+  selectChange(event: any, index: number){
+    let evt = parseInt(event.target.value)
+    this.arrayTrocas[index] = evt
+    this.calcularQtdDeTrocas()
+  }
+
+  calcularQtdDeTrocas(){
+    this.qtdProdutosTrocas = this.arrayTrocas.reduce((acumulador, valor) => acumulador + valor)
+  }
+
+
+  irParaProximaPagina(){
+    let qtdTotalProdutos = this.calcularQtdTotal()
+    if(qtdTotalProdutos > this.qtdProdutosTrocas){
+      this.router.navigate(['/finalizar-cartao']);
+    } else {
+      this.router.navigate(['/finalizar-endereco-cobranca']);
+    }
+  }
+
+  calcularQtdTotal(){
+    let qtd = 0
+    for(let i = 0; i < this.carrinho.carrinhoProduto!.length; i++){
+      qtd += this.carrinho.carrinhoProduto![i].quantidade
+    }
+    return qtd;
+  }
 
   backPage() {
     this.router.navigate(['/carrinho-compras']);
   }
-
 }
