ba9c528e68e36f50f7962841aad49479026a96bc
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-05-08 20:20:22 -0300

    Update dashboard-vendas.component.ts

--- src/app/modules/admin/funcionario/dashboard-vendas/dashboard-vendas.component.ts
+++ src/app/modules/admin/funcionario/dashboard-vendas/dashboard-vendas.component.ts
@@ -19,9 +19,8 @@ export class DashboardVendasComponent implements OnInit {
   ngOnInit(): void {
     this.validacao();
     google.charts.load('current', {'packages':['line']});
-    google.charts.setOnLoadCallback(this.drawChart);
   }
-  drawChart() {
+  /*drawChart() {
     var data = new google.visualization.DataTable();
     data.addColumn('number', 'Day');
     data.addColumn('number', 'Guardians of the Galaxy');
@@ -57,6 +56,53 @@ export class DashboardVendasComponent implements OnInit {
     var chart = new google.charts.Line(document.getElementById('linechart_material'));
 
     chart.draw(data, google.charts.Line.convertOptions(options));
+  }*/
+  buildChart(){
+    let renderChart = (chart: any)=>{
+    var data = new google.visualization.DataTable();
+
+    console.log(this.Resposta);
+    
+    data.addColumn('number', 'Day');
+    data.addColumn('number', 'Guardians of the Galaxy');
+    data.addColumn('number', 'The Avengers');
+    data.addColumn('number', 'Transformers: Age of Extinction');
+
+    data.addRows([
+      [1,  37.8, 80.8, 41.8],
+      [2,  30.9, 69.5, 32.4],
+      [3,  25.4,   57, 25.7],
+      [4,  11.7, 18.8, 10.5],
+      [5,  11.9, 17.6, 10.4],
+      [6,   8.8, 13.6,  7.7],
+      [7,   7.6, 12.3,  9.6],
+      [8,  12.3, 29.2, 10.6],
+      [9,  16.9, 42.9, 14.8],
+      [10, 12.8, 30.9, 11.6],
+      [11,  5.3,  7.9,  4.7],
+      [12,  6.6,  8.4,  5.2],
+      [13,  4.8,  6.3,  3.6],
+      [14,  4.2,  6.2,  3.4]
+    ]);
+    /*
+    var chartItems = [];
+    chartItems.push();*/
+
+    var options = {
+      chart: {
+        title: 'Categorias mais vendidas até o momento',
+        subtitle: 'Quantidade medida por unidades vendidas'
+      },
+      width: 900,
+      height: 500
+    };
+
+    chart().draw(data, google.charts.Line.convertOptions(options));
+    }
+
+    var linearChart = () => new google.charts.Line(document.getElementById('linechart_material'));
+    var callBack = () => renderChart(linearChart);
+    google.charts.setOnLoadCallback(callBack);
   }
 
   validacao(): void{
@@ -75,9 +121,10 @@ export class DashboardVendasComponent implements OnInit {
       (obj: any)=>{
         this.Resposta = obj;
         console.log(this.Resposta);
+        console.log(this.Resposta[0]);
+        this.buildChart();
       }
     );
-  
   }
 
   backPage() {
